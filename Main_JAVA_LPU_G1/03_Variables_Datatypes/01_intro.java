c987abfdf66b1d266822692ff255bd86be6fbc48
adhirajChauhan <adhirajsingh0105@gmail.com>
2024-01-16 20:50:07 +0530

    type conversion

--- Main_JAVA_LPU_G1/03_Variables_Datatypes/01_intro.java
+++ Main_JAVA_LPU_G1/03_Variables_Datatypes/01_intro.java
@@ -1,16 +1,29 @@
 class Intro{
     public static void main(String args[]){
 
-        int num1 = 5;
-        int num2 = 10;
-        int result = num1 + num2;
+        // int num1 = 5;
+        // int num2 = 10;
+        // int result = num1 + num2;
 
-        float num3 = 10.5f;
-        byte num4 = 11;
-        double num5 = 11.11111;
-        long num6 = 1321;
-        Boolean val = true;
-        System.out.println(val);
+        // float num3 = 10.5f;
+        // byte num4 = 11;
+        // double num5 = 11.11111;
+        // long num6 = 1321;
+        // Boolean val = true;
+        // System.out.println(val);
+
+
+        //Type Casting
+
+
+        // int myInt = 10;
+        // double myDouble = myInt; //automatic casting
+
+        double myDouble = 10.1234;
+        int myInt = (int) myDouble; 
+
+        System.out.println(myDouble);
+        System.out.println(myInt);
 
     }
 }
