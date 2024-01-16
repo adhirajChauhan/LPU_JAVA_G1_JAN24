94b2a54dc5afea6f41ef8e8342223b959effeaf5
adhirajChauhan <adhirajsingh0105@gmail.com>
2024-01-16 20:16:42 +0530

    Adding project files

---
+++ Main_JAVA_LPU_G1/03_Variables_Datatypes/01_intro.java
@@ -0,0 +1,17 @@
+class Intro{
+    public static void main(String args[]){
+
+        int num1 = 5;
+        int num2 = 10;
+        int result = num1 + num2;
+
+        float num3 = 10.5f;
+        byte num4 = 11;
+        double num5 = 11.11111;
+        long num6 = 1321;
+        Boolean val = true;
+        System.out.println(val);
+
+    }
+}
+
