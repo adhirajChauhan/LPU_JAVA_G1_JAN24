cadf4b9364c680d17a64cf2e205edffccf6fbb89
adhirajChauhan <adhirajsingh0105@gmail.com>
2024-01-22 20:56:13 +0530

    22012024

---
+++ Main_JAVA_LPU_G1/04_Operators/01_Intro.java
@@ -0,0 +1,54 @@
+// + - * / %
+
+//Create two variable, perform above mentioned operations and print the result
+
+import javax.swing.plaf.synth.SynthStyle;
+
+class LPU{
+    public static void main(String args[]){
+
+        //Arithematic operators - used to perform mathematical operations
+
+        // = -> Assignment operator
+
+        // int num1 = 10;
+        // int num2 = 5;
+
+        // System.out.println("Add -> " + (num1 + num2));
+        // System.out.println("Sub -> " + (num1 - num2));
+        // System.out.println("Mul -> " + (num1 * num2));
+        // System.out.println("Divide -> " + (num1 /num2));
+        // System.out.println("Modulus -> " + (num1 % num2));
+
+        // int num = 5;
+        // num = num + 1;
+        // num += 5;
+
+        // num++ -> post inceremnt
+        // ++num -> pre inceremnt
+
+        // int output = ++num;
+
+        // System.out.println(output);
+
+
+        //----------------------------------------------
+            //Comparison operators
+            // used to compare two values /variabes
+
+            // >  <  >=  <=  ==  !=
+        // int a = 5;
+        // int b = 7;
+
+        // System.out.println(a == b);
+
+        //----------------------------------------------
+            //Logical operators
+                // && || !
+        
+            int a = 5, b = 10;
+            int x = 3, y = 1;
+
+            System.out.println((a < b || x < y) && x > a);
+    }
+}
---
+++ Main_JAVA_LPU_G1/05_Conditional_Statements/01_If_Else.java
@@ -0,0 +1,28 @@
+class LPU{
+    public static void main(String args[]){
+        int num1 = 15;
+        // int num2 = 20;
+
+        if(num1 < 10){
+            System.out.println("A");
+        }
+        else if(num1 == 15){
+            System.out.println("B");
+
+        }
+        else{
+            System.out.println("C");
+
+        }
+    }
+}
+
+
+
+
+//Create two variables length and breadth, and check if it creates a square or not, print "It's a sqaure" if true otherwise print "It's not a square"
+
+
+//Write a program to print the absolute values of following numbers
+// -1
+// 1
