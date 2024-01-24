e95e0cbe4c702919a14f4739dfb34e794860cbb0
adhirajChauhan <adhirajsingh0105@gmail.com>
2024-01-24 19:54:35 +0530

    24012024

--- Main_JAVA_LPU_G1/06_Loops/02_for_loop.java
+++ Main_JAVA_LPU_G1/06_Loops/02_for_loop.java
@@ -1,13 +1,29 @@
 class LPU{
-    // public static void main(String args[])
-    // {
-    //     //for loop
+    public static void main(String args[])
+    {
+        //for loop
 
     //     for(int i = 0; i < 5; i++){
     //         System.out.println(("Hello World " + i));
     //     }
 
+    // for(int i = 0; i < 5; i++){
+
+    //     System.out.print(" Outer " + i);
+
+    //     for(int j = 0; j < 4; j++){
+    //         System.out.print("Inner " + j);
+    //     }
+
+    //     System.out.println("\n");
+
     // }
+
+
+    
+
+
+    }
 }
 
 
