a58c8cecf89cb94ea673cd315c0a14ed76993e1d
adhirajChauhan <adhirajsingh0105@gmail.com>
2024-01-30 19:10:13 +0530

    30012024

---
+++ Main_JAVA_LPU_G1/07_Arrays/01_Intro.java
@@ -0,0 +1,44 @@
+class LPU{
+    public static void main(String args[]){
+
+        //Arrays
+
+        // int arr[] = {1,2,3,4,5};
+        // System.out.println(arr[3]);
+
+        // int arr[] = new int[10];
+        // String arrString[] = new String[3];
+
+        // arr[0] = 10;
+        // arr[1] = 20;
+
+        // arrString[0] = "name1";
+        // System.out.println(arr[1]);
+        // System.out.println(arrString[0]);
+
+        // for(int i = 0; i < arr.length; i++){
+        //     System.out.println(arr[i]);
+        // }
+        
+        // for(int i : arr){
+        //     System.out.println(i);
+        // }
+        
+
+        int arr[][] = new int[5][4];
+
+    }
+}
+
+//Fast access -> it is very fast to accessa ny element in an array(by index of the elements), accessing the first element will take same time as accessing the last element.
+
+//Fixed size -> once created, we cannot change the size.
+
+
+
+//store names of your 5 friends in an array and print them
+
+//store roll numbers of your 5 friends in an array, if the numbers are odd, print them otherwise dont print
+
+
+//Create an array of 5 floats and claculate their sum and print
