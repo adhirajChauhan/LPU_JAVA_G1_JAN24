c0c886027da5d535c76b9d69d83fe1db5a188b67
adhirajChauhan <adhirajsingh0105@gmail.com>
2024-01-31 19:52:37 +0530

    31012024

---
+++ Main_JAVA_LPU_G1/07_Arrays/03_jagged.java
@@ -0,0 +1,39 @@
+class LPU{
+
+    public static void main(String a[]){
+
+        //jagged arrays
+
+        int arr[][] = new int[3][];
+
+        arr[0] = new int[4];
+        arr[1] = new int[3];
+        arr[2] = new int[2];
+
+        int value = 1;
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                arr[i][j] = value;
+                value++;
+            }
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                System.out.print(arr[i][j] + " ");
+                
+            }
+            System.out.println();
+
+        }
+
+        // int arr[][] = {{1,2},{3,4,5,6},{7,8,9}};
+
+        // for(int i = 0; i < arr.length; i++){
+        //     for(int j = 0; j < arr[i].length; j++){
+        //         System.out.print(arr[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+    }
+}
